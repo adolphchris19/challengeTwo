cc34c6d888777db18d925f98ee2a17dbd11f7ab3
adolphchris19 <adolphchris19@gmail.com>
2018-06-29 00:41:34 -0700

    added maths segment

--- app/src/main/java/com/example/android/randaquiz/ChemistryActivity.java
+++ app/src/main/java/com/example/android/randaquiz/ChemistryActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -25,6 +26,7 @@ public class ChemistryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chemistry);
+
 
     }//end of onCreate
 
